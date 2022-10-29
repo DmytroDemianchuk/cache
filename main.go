aa66be3d558dbf9cd2139af09b7260dd1423b705
dmytrodemiancuk <moqitee@gmail.com>
2022-10-29 21:08:31 +0200

    update

--- main.go
+++ main.go
@@ -10,12 +10,12 @@ func main() {
 	cache := cache.New()
 
 	cache.Set("userId", 42)
-	userId := cache.Get("userId")
+	userId, _ := cache.Get("userId")
 
 	fmt.Println(userId)
 
 	cache.Delete("userId")
-	userId := cache.Get("userId")
+	userId, _ = cache.Get("userId")
 
 	fmt.Println(userId)
 }
