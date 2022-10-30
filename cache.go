db37bf50a3fd64b0d1d86ff50b5a25b6ed8ddcf6
dmytrodemiancuk <moqitee@gmail.com>
2022-10-30 12:49:18 +0100

    update

--- cache.go
+++ cache.go
@@ -2,14 +2,17 @@ package cache
 
 import "errors"
 
+// Create struct with map
 type Cache struct {
 	items map[string]Item
 }
 
+// Create struct of value
 type Item struct {
 	Value interface{}
 }
 
+// Create constructor
 func New() *Cache {
 	items := make(map[string]Item)
 
@@ -20,20 +23,23 @@ func New() *Cache {
 	return &cache
 }
 
+// Add new item to cache
 func (c *Cache) Set(key string, value interface{}) {
 	c.items[key] = Item{
 		Value: value,
 	}
 }
 
+// Get item from cache storage
 func (c *Cache) Get(key string) (interface{}, error) {
 	item, exists := c.items[key]
 	if !exists {
-		return nil, errors.New("cacheItem with such key doesn't exist")
+		return nil, errors.New("cache element not found")
 	}
 	return item.Value, nil
 }
 
+// Delete item from cache storage
 func (c *Cache) Delete(key string) error {
 	if _, found := c.items[key]; !found {
 		return errors.New("key not found")
